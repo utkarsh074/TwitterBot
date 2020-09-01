d0ed9764eb4d1e6d3f24d5c66be70375618c4a74
Utkarsh Dixit <utkarsh074@gmail.com>
2020-09-01 22:29:53 +0530

    Update Deno.py
    Removed API Keys

--- Deno.py
+++ Deno.py
@@ -1,14 +1,13 @@
 import self as self
 import tweepy
 from FinalTweet import TweetFinal
-Access_token = '2515291648-kjyDSDUP2ZMFUP4grqhHB2k3knwX2Lv4bBTbjSS'
-Access_token_secret = 'NF4M1mh2u2J28BueqNQiHXtdWijAedW8u2UUHUfo7mcks'
+Access_token = ''
+Access_token_secret = ''
 
-Bearer_token = 'AAAAAAAAAAAAAAAAAAAAAHmfGgEAAAAAWUmllG5Ds%2BJXqYuywpe4yEzrimw' \
-               '%3DlJczyZlYlb1SntAnphZuLTgm9hjWzrPvDZWBu0XYotH8Q4ygRA'
+Bearer_token = ''
 
-CONSUMER_KEY = 'tkSxEEURnWFOlmB7Gejd0kpzY'
-CONSUMER_SECRET = 'LjfTPsA5xm48AHkWPqS95RVuS7f3HEqi5b6ORjIlDhAMidgMoS'
+CONSUMER_KEY = ''
+CONSUMER_SECRET = ''
 
 auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
 auth.set_access_token(Access_token, Access_token_secret)
